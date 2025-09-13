28a056a581f09e18c855f4523bd684e197172dd1
minhazulabedinsiam <freelancersiam@gmail.com>
2025-09-13 20:09:53 +0600

    design set

--- app/page.js
+++ app/page.js
@@ -1,22 +1,26 @@
 import React from 'react'
 import Image from 'next/image'
 import DarkLogo from '@/public/images/darklogo.png'
+import Header from '@/components/header'
 
 const home = () => {
   return (
     <>
       <div className='home'>
-        <div className='home-content'>
-          <div className='home-logo-wrap'>
-            <Image 
-              src={DarkLogo}
-              alt='Sky Turf Logo'
-              className='home-logo'
-            />
+        <div className="container">
+          <div className='home-content'>
+            <div className='home-logo-wrap'>
+              <Image 
+                src={DarkLogo}
+                alt='Sky Turf Logo'
+                className='home-logo'
+              />
+            </div>
+            <h2 className='home-title'>This site is under construction</h2>
           </div>
-          <h2 className='home-title'>This site is under construction</h2>
         </div>
       </div>
+      <Header />
     </>
   )
 }
