a8fa22450409951fe77d5279d6bea910aa0eff2c
minhazulabedinsiam <freelancersiam@gmail.com>
2025-10-15 15:12:45 +0600

    home page facilities components markup created

--- app/page.js
+++ app/page.js
@@ -5,27 +5,14 @@ import Hero from '@/components/Hero'
 import Turfs from '@/components/Turfs'
 import Map from '@/components/Map'
 import Gallery from '@/components/Gallery'
+import Facilites from '@/components/Facilites'
 
 const home = () => {
   return (
     <>
-      {/* <div className='home'>
-        <div className="container">
-          <div className='home-content'>
-            <div className='home-logo-wrap'>
-              <Image 
-                src={DarkLogo}
-                alt='Sky Turf Logo'
-                className='home-logo'
-              />
-            </div>
-            <h2 className='home-title'>This site is under construction</h2>
-          </div>
-        </div>
-      </div> */}
-
       <Hero />
       <Turfs />
+      <Facilites />
       <Map />
       <Gallery />
     </>
