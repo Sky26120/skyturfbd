f09a55e5f1d29146dc15db1d87c799bbdc71ce6d
minhazulabedinsiam <freelancersiam@gmail.com>
2025-12-31 01:27:22 +0600

    all design issue solved

--- components/dashboard/BookingTable.jsx
+++ components/dashboard/BookingTable.jsx
@@ -15,34 +15,37 @@ export default function BookingTable({ bookings, refetch }) {
   return (
     <div className="dashboard__booking">
       <p className="dashboard__booking-title">Booking</p>
-      <table>
-        <div className="dashboard__booking-list">
-          <thead>
-            <tr>
-              <th>Date</th>
-              <th>Slot</th>
-              <th>Name</th>
-              <th>Phone</th>
-              <th>Arena</th>
-              <th>Advance</th>
-              <th>Total</th>
-              {role !== "USER" && <th>Transaction ID</th>}
-              {role !== "USER" && <th>Status</th>}
-              {role !== "USER" && <th>Action</th>}
-            </tr>
-          </thead>
-          <tbody>
-            {bookings.map((booking) => (
-              <BookingTableRow
-                key={booking._id}
-                booking={booking}
-                role={role}
-                refetch={refetch}
-              />
-            ))}
-          </tbody>
-        </div> 
-      </table>
+      <div className="dashboard__booking-scroll">
+        <table>
+          <div className="dashboard__booking-list">
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Slot</th>
+                <th>Name</th>
+                <th>Phone</th>
+                <th>Arena</th>
+                <th>Advance</th>
+                <th>Total</th>
+                {role !== "USER" && <th>Transaction ID</th>}
+                {role !== "USER" && <th>Status</th>}
+                {role !== "USER" && <th>Action</th>}
+              </tr>
+            </thead>
+            <tbody>
+              {bookings.map((booking) => (
+                <BookingTableRow
+                  key={booking._id}
+                  booking={booking}
+                  role={role}
+                  refetch={refetch}
+                />
+              ))}
+            </tbody>
+          </div> 
+        </table>
+      </div>
+      
     </div>
   );
 }
